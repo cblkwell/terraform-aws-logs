848b064930da4e83f4785b98fdd9d32cf4705e1b
Chikara <chtakahashi10@gmail.com>
2021-10-21 17:02:29 -0400

    remove dod module

--- examples/guardduty/main.tf
+++ examples/guardduty/main.tf
@@ -9,13 +9,6 @@ module "aws_logs" {
   force_destroy = var.force_destroy
 }
 
-module "guardduty" {
-  source  = "dod-iac/guardduty/aws"
-  version = "~> 1"
-
-  s3_bucket_name = module.aws_logs.aws_logs_bucket
-}
-
 /*
 The MIT License (MIT)
 
